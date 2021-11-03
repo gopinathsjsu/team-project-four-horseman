1dac38ad3a0237d3754812a0239af007cc7564f9
Pratik Dabre <80276547+pdabre12@users.noreply.github.com>
2021-11-03 01:27:44 -0700

    Add files via upload
    Remaining components of SignIn page

---
+++ frontend/src/components/RegisterPage/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavigationBar = () => {
+    return (
+        <div >
+            <div className='navigation-bar'></div>
+        </div>
+    )
+}
+
+export default NavigationBar
---
+++ frontend/src/components/RegisterPage/Register.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import NavigationBar from './NavigationBar'
+import PersonalInfo from './PersonalInfo'
+const Register = () => {
+    return (
+        <div>
+                  <NavigationBar/>
+
+            <Header />
+      <PersonalInfo />
+        </div>
+    )
+}
+
+export default Register
