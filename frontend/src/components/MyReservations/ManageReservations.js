9d745355e053e20a362841aeff5a5ad140965e89
Pratik Dabre <80276547+pdabre12@users.noreply.github.com>
2021-11-19 18:22:08 -0800

    Create ManageReservations.js

---
+++ frontend/src/components/MyReservations/ManageReservations.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ManageReservations = () => {
+    return (
+        <div>
+            <div className='manage-reservations'>
+            <button style={{marginLeft:'25%'}}>Cancel booking</button>
+            <button style={{marginLeft:'2%'}}>Change booking</button>
+        </div>
+    
+        </div>
+    )
+}
+
+export default ManageReservations
