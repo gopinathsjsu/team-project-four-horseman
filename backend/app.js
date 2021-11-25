0f28ab7af441648906fbe36ff0c2a59b6954bfef
Siddharth Sircar <sidsircar16@gmail.com>
2021-11-25 13:42:10 -0800

    added CORS

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bp = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bp.json());
@@ -8,6 +9,18 @@ app.use(
     extended: true,
   })
 );
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Welcome to FH Airlines");
 });
