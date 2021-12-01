357eeb0fb122d672e5201fb54b67ab3253ee0364
Unmesh Padhye <unmesh@Unmeshs-MacBook-Pro.local>
2021-11-30 19:33:59 -0800

    Added local storage var for storing user Object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import FindReservations from "./components/ManageReservations/FindReservations";
 import BookFlights from "./components/BookFlights/BookFlights";
 import MyReservations from "./components/MyReservations/MyReservations";
 import DisplayFlights from "./components/DisplayFlights/DisplayFlights";
-
 import UserProfile from "./components/UserProfile/UserProfile";
 import LoginPage from "./components/LoginPage/Login";
 import SingleFlightDetails from "./components/DisplayFlights/SingleFlightDetails";
