ed7772848848666aab3b0e52cb2d42c76e6c0d38
Unmesh Padhye <unmesh@Unmeshs-MacBook-Pro.local>
2021-11-30 20:22:44 -0800

    User profile card created

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import useForm from "./useForm";
 import { get } from "../../utils/Api";
+import { Redirect } from "react-router";
 
 const UserProfile = (props) => {
   const [values, handleChange] = useForm({
@@ -33,36 +34,34 @@ const UserProfile = (props) => {
     } */
   };
 
+  const BookFlights = () => {
+    <Redirect to="/" />;
+  };
+
   return (
     <>
       {userData ? (
-        <div className="userprofile">
-          <br />
-          <h1 class="display-1">Welcome onboard, Happy flying!</h1>
-          <p>
-            Name: {userData.firstName} {userData.lastName}
+        <div>
+          <div className="userprofile-card">
             <br />
-            Address: {userData.address}
-            <br />
-            Home City: {userData.city}
-            <br />
-            State: {userData.state}
-            <br />
-            Zip Code: {userData.zip}
-            <br />
-            Country: Email: {userData.email}
-            <br />
-            Miles Balance: {userData.miles}
-          </p>
-
-          <input
-            onClick={(e) => getUserDetails(e)}
-            type="submit"
-            className="signin-submit-button"
-          ></input>
-          <br />
-          <br />
-          <br />
+            <h1 class="userprofile-title">Welcome onboard, Happy flying!</h1>
+            <p className="user">
+              Name: {userData.firstName} {userData.lastName}
+              <br />
+              Address: {userData.address}
+              <br />
+              Home City: {userData.city}
+              <br />
+              State: {userData.state}
+              <br />
+              Zip Code: {userData.zip}
+              <br />
+              Country: Email: {userData.country}
+              <br />
+              Miles Balance: {userData.miles}
+            </p>
+          </div>
+          <button onClick={BookFlights}>Book Flight</button>
         </div>
       ) : null}
     </>
