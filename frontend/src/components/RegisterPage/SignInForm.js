d5a30eba37dc24a624698d242be5e85fe6a30eba
Unmesh Padhye <unmesh@Unmeshs-MacBook-Pro.local>
2021-11-30 18:56:57 -0800

    User data fetched and displayed in UserProfile if the user is logged in

--- frontend/src/components/RegisterPage/SignInForm.js
+++ frontend/src/components/RegisterPage/SignInForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import useForm from "./useForm";
+import { post } from "../../utils/Api";
 
 const SignInForm = () => {
   const [values, handleChange] = useForm({
